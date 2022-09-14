0b05a5ab4477624feaa40a199368d45a5a739194
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-09-13 23:06:13 -0500

    created form

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes)
+
 
 // https://www.mongodb.com/cloud/atlas
 const CONNECTION_URL = 'mongodb+srv://ryanmccutcheon21:Raiden032021@cluster0.taygszv.mongodb.net/?retryWrites=true&w=majority'
