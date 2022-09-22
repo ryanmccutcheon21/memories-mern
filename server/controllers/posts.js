97a789b85ef90722ed672947a60faae0ecdce085
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-09-22 01:18:22 -0500

    Initial front-end

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,27 @@
-// create all route handlers
+// import PostMessage
+// gives us access to our model
+import PostMessage from "../models/postMessage"
 
-export const getPosts = (req, res) => {
-    res.send('This works!')
+// create all route handlers
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find()
+
+        res.status(200).json(postMessages)
+    } catch (e) {
+        res.status(404).json({ message: e.message })
+    }
+}
+
+export const createPosts = async (req, res) => {
+    const post = req.body
+
+    const newPost = new PostMessage(post)
+
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch (e) {
+        res.status(409).json({ message: e.message })
+    }
 }
