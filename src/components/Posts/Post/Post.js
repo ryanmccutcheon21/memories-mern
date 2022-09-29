fae1e6baba9d02f481079be492b560dad6413527
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-09-28 19:00:59 -0500

    Adding like post functionality

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -39,9 +39,9 @@ const Post = ({ post, setCurrentId }) => {
                 </Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size='small' color='primary' onClick={() => { }}>
+                <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize='small' />
-                    Like
+                    Like&nbsp;
                     {post.likeCount}
                 </Button>
                 <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
