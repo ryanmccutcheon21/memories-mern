5eb66fdd944250b8d39a34daa4b88a9cfa217900
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-10-03 09:41:57 -0500

    Add .env files

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 // import routes
 import postRoutes from './routes/posts.js';
 
 const app = express()
+
+// call dotenv to use in app
+dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -17,9 +21,8 @@ app.use(cors())
 // localhost:3000/posts
 app.use('/posts', postRoutes)
 
-// const CONNECTION_URL = process.env.REACT_APP_CONNECTION_URL;
-const CONNECTION_URL = 'mongodb+srv://ryanmccutcheon21:Raiden032021@memories-app.7dnxlv6.mongodb.net/?retryWrites=true&w=majority';
-const PORT = 5500;
+const CONNECTION_URL = process.env.REACT_APP_CONNECTION_URL;
+const PORT = process.env.REACT_APP_PORT || 5500;
 
 // use mongoose to connect to MongoDB
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
